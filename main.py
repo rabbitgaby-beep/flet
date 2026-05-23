753393c48559609d3659f540b1d1a9d23f9ecaaf
rabbitgaby-beep <rabbitgaby@gmail.com>
2026-05-23 00:21:13 -0300

    uno

---
+++ main.py
@@ -0,0 +1,12 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet Buttons Demo"
+    page.window.width = 200
+    page.window.height = 200
+
+    page.add(ft.ElevatedButton("Elevated Button"))
+    page.add(ft.FilledButton("Filled Button"))
+    page.add(ft.FloatingActionButton(icon=ft.Icons.ADD))
+
+ft.app(target=main)
